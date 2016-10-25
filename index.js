cf1b82f9fc2c80c32d0b73aef112888db38bdca2
Kevan Atkins <kevan.atkins@gmail.com>
2016-10-26 02:42:10 +1100

    A whole bunch of changes that should be separate commits... templates, authentication, refactoring. Pffff.

--- index.js
+++ index.js
@@ -1,17 +1,79 @@
 var config = require('./config');
 var express = require('express');
-var app = module.exports = express();
+var app = express();
 var logger = require('morgan');
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var AWS = require('aws-sdk');
 var path = require('path');
+var mustache = require('mustache');
+var fs = require('fs');
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
+var db = require('./db');
 
 var debug = function() {
   if (config.debug) {
     console.log.apply(console, arguments);
   }
 };
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// Configure the local strategy for use by Passport.
+//
+// The local strategy require a `verify` function which receives the credentials
+// (`username` and `password`) submitted by the user.  The function must verify
+// that the password is correct and then invoke `cb` with a user object, which
+// will be set at `req.user` in route handlers after authentication.
+passport.use(new Strategy(
+  function(username, password, cb) {
+    db.users.findByUsername(username, function(err, user) {
+      if (err) {
+        return cb(err);
+      }
+      if (!user) {
+        return cb(null, false);
+      }
+      if (user.password != password) {
+        return cb(null, false);
+      }
+      return cb(null, user);
+    });
+  }));
+
+
+// Configure Passport authenticated session persistence.
+//
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.  The
+// typical implementation of this is as simple as supplying the user ID when
+// serializing, and querying the user record by ID from the database when
+// deserializing.
+passport.serializeUser(function(user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+  db.users.findById(id, function(err, user) {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, user);
+  });
+});
+
+
+
 
 // Loading in the secret stuff
 AWS.config.loadFromPath('./aws-config.json');
@@ -25,9 +87,10 @@ io.on('connection', function(socket) {
     console.log("Querying...");
 
     var listScope = {
-      "eoi-project": "submissionID, #ts, artistName, project_title, focus_areas",
-      "eoi-fixed-media": "",
-      "eoi-collab": ""
+      "eoi-project": "submissionID, submissionType, #ts, artistName, project_title, focus_areas",
+      "eoi-fixed-media": "submissionID, submissionType, #ts",
+      "eoi-collab": "submissionID, submissionType, #ts, artistName",
+      "eoi-academy": "submissionID, submissionType, #ts"
     }
 
     var params = {
@@ -69,7 +132,35 @@ io.on('connection', function(socket) {
     }
   });
   socket.on('getSubmission', function(submissionID) {
-    //
+    var params = {
+      TableName: "tilde-submissions",
+      Key: {
+        "submissionID": submissionID
+      }
+    };
+
+    docClient.get(params, function(err, data) {
+      var htmlTemplates = {
+        "eoi-project": "project-submission.html.mustache",
+        "eoi-collab": "collab-submission.html.mustache",
+        "eoi-fixed-media": "fixed-media-submission.html.mustache",
+        "eoi-academy": "academy-submission.html.mustache"
+      }
+      if (err) {
+        console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+      } else {
+        console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+        d = new Date(data.Item.timestamp);
+        data.Item.timestamp = d.toDateString();
+        if (data.Item.cv.fname) {
+          var cvFileExt = data.Item.cv.fname.split('.').pop();
+          data.Item.cv_filename = data.Item.submissionID + '-cv.' + cvFileExt;
+        }
+        var htmlTemplate = fs.readFileSync('./mustache/' + htmlTemplates[data.Item.submissionType], 'utf8');
+        var htmlOutput = mustache.render(htmlTemplate, data.Item);
+        socket.emit('submissionEntry', htmlOutput);
+      }
+    });
   });
 
   socket.on('disconnect', function() {
@@ -83,10 +174,34 @@ app.use(logger('dev'));
 // serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// send index.html
-app.get('/', function(req, res) {
-  res.sendfile(__dirname + '/index.html');
-});
+// // send index.html
+// app.get('/', function(req, res) {
+//   res.sendfile(__dirname + '/index.html');
+// });
+
+app.get('/',
+  function(req, res) {
+    res.render('index', { user: req.user });
+  });
+
+app.get('/login',
+  function(req, res) {
+    res.render('login');
+  });
+
+app.post('/login',
+  passport.authenticate('local', {
+    failureRedirect: '/login'
+  }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/logout',
+  function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
 
 // start the apps
 server.listen(config.port, function() {
--- public/app.js
+++ public/app.js
@@ -5,6 +5,17 @@
   socket.on('connect', function () {
     console.log('connected to WebSocket at', socket.io.uri);
     socket.emit('listSubmissions', { submissionType: 'eoi-project' });
+    $("#submissionTypeSelector").on('change', function() {
+      var selectorValue = $(this).val();
+      var tableHeaders = {
+        "eoi-project": "Project Title",
+        "eoi-collab": "Artist Name",
+        "eoi-fixed-media": "Submission Title",
+        "eoi-academy": "Student Name"
+      };
+      $('#submissionList thead th:nth-child(2)').html(tableHeaders[selectorValue]);
+      socket.emit('listSubmissions', { submissionType: selectorValue });
+    });
   });
 
   socket.on('submissionList', function(retrievedSubmissions) {
@@ -13,15 +24,31 @@
       $('#submissionList').find("tbody").append("<tr id='" + submission.submissionID + "'></tr>")
       var submissionRow = $('#'+submission.submissionID);
       var d = new Date(submission.timestamp);
-      submissionRow.append("<td colspan='1'>"+d.toDateString()+"</td>");
-      submissionRow.append("<td colspan='4'><a class='submissionLink' href='#' data-submission-id='"+submission.submissionID+"'>"+submission.submissionID+"</a></td>");
-      submissionRow.append("<td colspan='7'>"+submission.project_title+"</td>");
+      submissionRow.append("<td colspan='2'>"+d.toDateString()+"</td>");
+      switch (submission.submissionType) {
+        case "eoi-project":
+          submissionRow.append("<td colspan='10'><a class='submissionLink' href='#' data-submission-id='"+submission.submissionID+"'>"+submission.project_title+"</a><br><small>"+submission.submissionID+"</small></td>");
+          break;
+        case "eoi-collab":
+          submissionRow.append("<td colspan='10'><a class='submissionLink' href='#' data-submission-id='"+submission.submissionID+"'>"+submission.artistName+"</a><br><small>"+submission.submissionID+"</small></td>");
+          break;
+        case "eoi-fixed-media":
+          submissionRow.append("<td colspan='10'><a class='submissionLink' href='#' data-submission-id='"+submission.submissionID+"'>Submission Link</a><br><small>"+submission.submissionID+"</small></td>");
+          break;
+        case "eoi-academy":
+          submissionRow.append("<td colspan='10'><a class='submissionLink' href='#' data-submission-id='"+submission.submissionID+"'>"+submission.artistName+"</a><br><small>"+submission.submissionID+"</small></td>");
+          break;
+      }
     });
     $('.submissionLink').on('click', function() {
       var linkID = $(this).data("submission-id");
-      
+      socket.emit('getSubmission', linkID);
     });
   });
+
+  socket.on('submissionEntry', function(content) {
+    $.featherlight($('#mylightbox'), {html: content});
+  })
 
   socket.on('disconnect', function () {
     console.log('socket disconnected');
